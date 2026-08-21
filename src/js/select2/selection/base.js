9d1da0793e19b702860fab0850834b804395a1b4
Luigi Teschio <gigitux@gmail.com>
2026-08-21 18:39:26 +0200

    Fix legacy QUnit test harness

--- src/js/select2/selection/base.js
+++ src/js/select2/selection/base.js
@@ -15,7 +15,7 @@ define([
   BaseSelection.prototype.render = function () {
     var $selection = $(
       '<span class="select2-selection" ' +
-      ' aria-haspopup="true" aria-expanded="false">' +
+      ' role="combobox" aria-haspopup="true" aria-expanded="false">' +
       '</span>'
     );
 
